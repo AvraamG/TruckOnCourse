c83a5f4d940c308fb2cad2bfdd5ce08c9dbce312
AvraamGeorgiadis <georgiadis.ab@gmail.com>
2020-07-17 22:06:43 +0200

    Wrap up for week 1.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float distanceThreshold;
+
+    [SerializeField]
+    Transform playerTransform;
+
+    Vector3 currentOffset = new Vector3(0, 5, -7);
+
+    //TODO 
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Follow();
+    }
+
+
+    private void Follow()
+    {
+        float distance = Vector3.Distance(this.transform.position, playerTransform.position);
+        if (distance > distanceThreshold)
+        {
+          transform.position = playerTransform.transform.position + currentOffset;
+          transform.rotation = playerTransform.rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/Vehicle/IVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ISteerable
+{
+    void Steer(float steeringSpeed);
+}
+
+public interface IMovable
+{
+    void Move(float movementSpeed);
+
+}
+public interface IRecyclable
+{
+    void Recycle();
+
+}
+    
+//This is a generic interface where T is a placeholder
+//for a data type that will be provided by the 
+//implementing class.
+public interface IDamageable<T>
+{
+    void TakeDamage(T damageTaken);
+}
